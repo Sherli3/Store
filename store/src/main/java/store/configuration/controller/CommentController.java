7a3c7d03874bd13c48b9e6e66c39d877a0e3da6b
Anastasiya Klimkova <anastasiya.klimkovaa@gmail.com>
2019-09-16 00:51:44 +0300

    +add controller edit and delete comment

--- store/src/main/java/store/configuration/controller/CommentController.java
+++ store/src/main/java/store/configuration/controller/CommentController.java
@@ -2,6 +2,7 @@ package store.configuration.controller;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -10,10 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import store.configuration.model.Comment;
@@ -48,18 +49,45 @@ public class CommentController {
 	}
 
 	@RequestMapping(path = "/addComment", method = RequestMethod.POST)
-	public String saveNewComment(@Valid Comment newComment, @RequestParam(defaultValue = "false") Boolean approved,
-			BindingResult result, Model model) {
+	public String saveNewComment(@Valid Comment newComment, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			return "error";
 		}
 		long time = date.getTime();
 		Timestamp ts = new Timestamp(time);
 		newComment.setCreatedAt(ts);
-		newComment.setApproved(approved);
+		newComment.setApproved(false);
 		commentService.saveComment(newComment);
 
 		return "redirect:/games/list/";
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editGameGet(@PathVariable("id") Integer id, Model model) {
+		commentService.getCommentById(id).ifPresent(comment -> model.addAttribute("comment", comment));
+		return "comment-edit";
+	}
+
+	@RequestMapping(path = "/edit/{id}", method = RequestMethod.POST)
+	public String editComment(@Valid @ModelAttribute("comment") Comment comment, BindingResult result) {
+		if (result.hasErrors()) {
+			return "error";
+		}
+		commentService.saveComment(comment);
+		return "redirect:/object/list/";
+	}
+
+	@RequestMapping(path = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteCommen(@PathVariable("id") Integer id) {
+		Optional<Comment> comment = commentService.getCommentById(id);
+		comment.ifPresent(c -> commentService.deleteComment(c.getId()));
+		return "redirect:/object/list/";
+	}
+	@RequestMapping("/list")
+	public String listGames(Model model) {
+		List<Comment> listComment = commentService.findAllApprovedComments();
+		model.addAttribute("listComment", listComment);
+		return "comment-list";
 	}
 
 }
