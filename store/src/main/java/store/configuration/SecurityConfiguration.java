5d6fc2c58cac67ea8b1523f1c9c7c83adf3751ce
Anastasiya Klimkova <anastasiya.klimkovaa@gmail.com>
2019-09-17 10:13:02 +0300

    + class SecurityConfiguration

---
+++ store/src/main/java/store/configuration/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package store.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("store.configuration.service")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	};
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/games/list", "/user/login", "/user/registration").permitAll()
+				.antMatchers("/user/**").hasAuthority("USER").antMatchers("/trader/**").hasAnyAuthority("TRADER")
+				.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest().authenticated().and().formLogin()
+				.loginPage("/user/login").failureUrl("/login?error=true").defaultSuccessUrl("/user/cabinet", true)
+				.usernameParameter("email").passwordParameter("password").and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/games/list").and()
+				.exceptionHandling().accessDeniedPage("/access-denied");
+		http.csrf().disable();
+
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+}
