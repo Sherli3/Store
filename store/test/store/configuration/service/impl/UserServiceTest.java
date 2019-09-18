ad160988a33b4d2d256c688441348fce5c95b0fb
Anastasiya Klimkova <anastasiya.klimkovaa@gmail.com>
2019-09-18 15:13:20 +0300

    changes in view

---
+++ store/test/store/configuration/service/impl/UserServiceTest.java
@@ -0,0 +1,10 @@
+package store.configuration.service.impl;
+
+import org.junit.runner.RunWith;
+//import org.mockito.junit.MockitoJUnitRunner;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest  {
+
+
+}
