8e3fbaaddbd977ae04660389a0794d8257ab04fb
Anastasiya Klimkova <anastasiya.klimkovaa@gmail.com>
2019-09-18 15:12:12 +0300

    +mail sendler

--- store/src/main/java/store/configuration/service/impl/UserService.java
+++ store/src/main/java/store/configuration/service/impl/UserService.java
@@ -2,11 +2,13 @@ package store.configuration.service.impl;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import store.configuration.model.Role;
 import store.configuration.model.User;
@@ -22,6 +24,8 @@ public class UserService {
 	private RoleRepository roleRepository;
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private MailSender mailSender;
 
 	public User saveUser(User user) {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
@@ -30,7 +34,28 @@ public class UserService {
 			userRole = roleRepository.save(new Role("USER"));
 		}
 		user.setUserRoles(new HashSet<Role>(Arrays.asList(userRole)));
+		user.setActiveCode(UUID.randomUUID().toString());
+		if (!StringUtils.isEmpty(user.getEmail())) {
+			String message = String.format(
+					"Hello, %s! \n"
+							+ "Welcome to Store. Please, congiger link: http://localhost:8080/store/user/activate/%s",
+					user.getFirstName(), user.getActiveCode());
+
+			mailSender.send(user.getEmail(), "Activation code", message);
+		}
 		return userRepository.save(user);
+
+	}
+
+	public boolean activateUser(String code) {
+		User user = userRepository.findByActiveCode(code);
+
+		if (user == null) {
+			return false;
+		}
+		user.setActiveCode(null);
+		userRepository.save(user);
+		return true;
 	}
 
 	public User findByEmail(String email) {
